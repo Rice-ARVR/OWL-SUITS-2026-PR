a9e03dc566b384b2caf594f668b4c62fedb356a3
sumodha <pokhrel.sumodha@gmail.com>
2026-04-25 16:15:41 +0000

    feat: create global css file

--- client/app/routes.ts
+++ client/app/routes.ts
@@ -5,4 +5,5 @@ export default [
     route("tss_example", "routes/tss_example.tsx"),
     route("wasd_controls", "routes/wasd_controls.tsx"),
     route("gamepad_controls", "routes/gamepad_controls.tsx"),
+    route("ui", "routes/ui.tsx")
 ] satisfies RouteConfig;
