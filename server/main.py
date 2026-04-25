df10780f43708d1495c574b8434dd748a4fdcc20
Israel Cantu <25886371+Izzy129@users.noreply.github.com>
2026-04-25 10:21:38 -0500

    chore: fix leftover git conflict text

--- server/main.py
+++ server/main.py
@@ -6,10 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routers.tss_example import router as tss_example_router
 from app.routers.locations import router as locations_router
 from app.routers.warnings import router as warnings_router
-<<<<<<< HEAD
 from app.routers.rover_control import router as rover_control_router
-=======
->>>>>>> 29c0ad7a275e5dcef28b1acd66d68441b29e08d4
 from app.routers.vital_signs import router as vital_signs_router
 from app.services.telemetry.telemetry_service import start_polling, stop_polling
 from app.db.database import connect, disconnect
@@ -36,8 +33,5 @@ app.add_middleware(
 app.include_router(tss_example_router)
 app.include_router(locations_router)
 app.include_router(warnings_router)
-<<<<<<< HEAD
 app.include_router(rover_control_router)
-=======
->>>>>>> 29c0ad7a275e5dcef28b1acd66d68441b29e08d4
 app.include_router(vital_signs_router)
